1357d453644edded800ca6a175d9539d37ff06d5
Reggie Wilcox <reginawilcox@Reginas-MacBook-Pro.local>
2021-09-15 13:51:51 -0400

    hello fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+print("bruh wtf")
+for num in range(1,100):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
